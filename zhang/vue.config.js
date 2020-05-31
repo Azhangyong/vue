0e7f4fda0053702f9a85e900bbb52ea41f94b31c
zhang <2832499@qq.com>
2020-05-31 21:03:06 +0800

    x

--- zhang/vue.config.js
+++ zhang/vue.config.js
@@ -19,16 +19,16 @@ module.exports = {
     configureWebpack: (config) => {
         config.resolve = { // 配置解析别名
             extensions: ['.js', '.json', '.vue'],
-            // alias: {
-            //     '@': path.resolve(__dirname, './src'),
-            //     'components': path.resolve(__dirname, './src/components'),
-            //     'common': path.resolve(__dirname, './src/common'),
-            //     'api': path.resolve(__dirname, './src/api'),
-            //     'router': path.resolve(__dirname, './src/router'),
-            //     'views': path.resolve(__dirname, './src/views'),
-            //     'data': path.resolve(__dirname, './src/data'),
-            //     'public': path.resolve(__dirname, 'public')
-            // }
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+                'components': path.resolve(__dirname, './src/components'),
+                'common': path.resolve(__dirname, './src/common'),
+                'api': path.resolve(__dirname, './src/api'),
+                'router': path.resolve(__dirname, './src/router'),
+                'views': path.resolve(__dirname, './src/views'),
+                'data': path.resolve(__dirname, './src/data'),
+                'public': path.resolve(__dirname, 'public')
+            }
         }
     },
     // 生产环境是否生成 sourceMap 文件
